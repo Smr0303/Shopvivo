22f66fa926a586dba595054afa355fcc0c13af08
Smr0303 <samarmohite7@gmail.com>
2024-06-30 17:33:59 +0530

    Error resolved

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 const errorMiddleware = require("./middleware/error");
 
-config
+
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
